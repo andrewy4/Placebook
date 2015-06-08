12a518e749ce3a7beaa6cfe8c4b52254ed744e2e
unknown <cztran@uci.edu>
2015-06-07 22:21:49 -0700

    Camera working, some bugs to fix

--- Placebook/app/src/main/java/andrewyu/placebook/MainActivity.java
+++ Placebook/app/src/main/java/andrewyu/placebook/MainActivity.java
@@ -1,19 +1,90 @@
 package andrewyu.placebook;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+        private static String logtag = "CameraTest";
+        private static int TAKE_PICTURE = 1;
+        private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cameraButton = (Button)findViewById(R.id.button_camera);
+        cameraButton.setOnClickListener(cameraListener);
     }
 
+    private View.OnClickListener cameraListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            //open the camera
+            takePhoto(view);
+        }
+    };
+
+            private void takePhoto(View view){
+                // when we take the photo we also need to get the location, will need to add another function
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"myPicture");
+                //need to figure out a way to not over write the photo worry about that later
+
+                imageUri = Uri.fromFile(photo);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PICTURE); //1 for rear camera
+
+
+
+            }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode,resultCode,intent);
+        if(resultCode == Activity.RESULT_OK){
+            Uri selectedImage = imageUri;
+            getContentResolver().notifyChange(selectedImage, null);// notify other apps, get everything on the same page
+
+            ImageView imageView = (ImageView)findViewById(R.id.image_camera);
+            ContentResolver cr = getContentResolver();
+            Bitmap bitmap;
+
+            //when working with data, try to a try block to catch errors
+
+            try{
+                //try to get bitmap data
+                bitmap = MediaStore.Images.Media.getBitmap(cr,selectedImage);
+                imageView.setImageBitmap(bitmap);
+                //send the app user a message
+                Toast.makeText(MainActivity.this,selectedImage.toString(), Toast.LENGTH_LONG).show();
+
+            }catch(Exception e){
+                Log.e(logtag, e.toString());
+            }
+        }
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,18 +93,4 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
